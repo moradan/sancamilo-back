082c7cbf236929d325cd6b7b28bfdaeff93f9376
moradan <realmoradan@hotmail.com>
2024-06-07 01:18:22 -0300

    cargo archivo al arrancar
    este servidor carga el archivo html que va a servir al momento de inicializar el servidor.
    
    (cherry picked from commit 9cd7b07968d07227a5c6359e265972565a16fe69)

--- server.js
+++ server.js
@@ -8,7 +8,30 @@ const rutaArchivo = path.join(__dirname, "../sancamilo-front/index.html");
 const indiceHTML = cargarArchivo();
 
 /**
- *
+ * @description el objeto servidor que se va a utilizar para controlar el lifecycle y la configuracion del servidor.
+ * @argument {http.RequestListener} servirFront
+ */
+const server = http.createServer(servirFront);
+
+/**
+ * @description comienza a escuchar pedidos en el puerto indicado por el argumento PORT.
+ * @argument {() => void | undefined} inicializar es un callback que se llama una vez que el servidor ha comenzado a escuchar
+ * pedidos en el puerto indicado.
+ */
+server.listen(PORT, inicializar);
+
+function cargarArchivo() {
+  try {
+    return fs.readFileSync(rutaArchivo);
+  } catch (/** @type {Error} */ error) {
+    if (error.code === "ENOENT") {
+      return undefined;
+    }
+  }
+}
+
+/**
+ * @description Se usa para simular un servidor que no esta disponible.
  * @param {http.IncomingMessage} req
  * @param {http.ServerResponse} res
  */
@@ -40,24 +63,20 @@ function noEncontrado(req, res) {
 function servirFront(req, res) {
   if (MANTENIMIENTO) {
     mantenimiento(req, res);
-    return;
+  }
+  if (!indiceHTML) {
+    noEncontrado(req, res);
   }
 
-  const rutaArchivo = path.join(__dirname, "../sancamilo-front/index.html");
-  try {
-    const indiceHTML = fs.readFileSync(rutaArchivo);
-    res.setHeader("Content-Type", "text/html");
-    res.writeHead(200);
-    res.end(indiceHTML);
-  } catch (/** @type {Error} */ error) {
-    if (error.code === "ENOENT") {
-      noEncontrado(req, res);
-    }
-  }
+  res.setHeader("Content-Type", "text/html");
+  res.writeHead(200);
+  res.end(indiceHTML);
 }
 
+/**
+ * @description Inicializa el servidor. Por el momento lo unico que hace es
+ * mostrar un mensaje por consola para confirmar que el servidor ya inicio.
+ */
 function inicializar() {
   console.log(`Servidor escuchando en puerto ${PORT}`);
 }
-
-server.listen(PORT, inicializar);
