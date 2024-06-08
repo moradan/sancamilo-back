1408465f76ae1acb5bbe39dc6d2b4b8a9618c16e
moradan <realmoradan@hotmail.com>
2024-06-08 13:14:46 -0300

    Clase FormularioLogin
    Defino las primeras propiedades de la clase FormularioLogin.
    Este es un primer boceto y puede cambiar varias veces.

--- src/formulario.js
+++ src/formulario.js
@@ -1,4 +1,21 @@
+/* La clase Formulario contendra el comportamiento comun a todos los formularios. Cada clase de formulario debe implementar unicamente aquellas
+cosas en las que se diferencia de todos los demas */
 export class Formulario {}
-export class FormularioLogin extends Formulario {}
+
+/**
+ * @description Contiene todos los datos provenientes de un pedido de log in del front end.
+ */
+export class FormularioLogin extends Formulario {
+  /**
+   *
+   * @param {string} nombreUsuario
+   * @param {string} contrasenia
+   */
+  constructor(nombreUsuario, contrasenia) {
+    this.nombreUsuario = nombreUsuario;
+    this.contrasenia = contrasenia;
+  }
+}
+
 export class FormularioRegistrarse extends Formulario {}
 export class FormularioTurno extends Formulario {}
