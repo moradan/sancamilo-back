91d5e5036749813eda72d072a81b7735791732a0
moradan <realmoradan@hotmail.com>
2024-06-09 20:30:12 -0300

    Incorporo los modulos procesadores a la clase Formulario.

--- src/formulario.js
+++ src/formulario.js
@@ -1,19 +1,38 @@
 import url from "url";
+import login from "./login.js";
+import registrarse from "./registrarse.js";
+import turnos from "./turnos.js";
 
 /**
  * Formulario base para derivar los demas formularios.
  */
-export class Formulario {
+export default class Formulario {
   /**
    * @type {url.UrlWithParsedQuery}
    */
   #pedido;
 
   /**
+   * @type {Function} Una funcion que se usará para procesar el formulario. Se debe elegir desde una lista de procesadores
+   * contenidos en modulos JS. A saber: login.js, registrarse.js, turnos.js.
+   */
+  #procesador;
+
+  /**
+   * Este mapa de procesadores almacena todos los procesadores que tenemos junto con el nombre del archivo html
+   * cuyo contenido maneja este procesador. De este modo, podemos usar el nombre del end point que esta contenido en
+   * el pedido http para definir cual procesador se va a usar para procesar la peticion.
+   * @type {Map<string, Function>}
+   */
+  #procesadores = new Map();
+
+  /**
    * @param {url.UrlWithParsedQuery}
    */
   constructor(pedido) {
     this.#pedido = pedido;
+    this.#procesadores.set("/src/inicioapp.html", login);
+    this.#procesador = this.#procesadores.get(this.#pedido.pathname);
   }
 
   /**
