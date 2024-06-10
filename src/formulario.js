b40b31111bc40c77b0a54d9bd13d0b8d2fb6040b
moradan <realmoradan@hotmail.com>
2024-06-10 10:00:01 -0300

    install ESLint

--- src/formulario.js
+++ src/formulario.js
@@ -27,7 +27,7 @@ export default class Formulario {
   #procesadores = new Map();
 
   /**
-   * @param {url.UrlWithParsedQuery}
+   * @param {url.UrlWithParsedQuery} pedido Este objeto organiza los distintos datos que contiene un url en propiedades.
    */
   constructor(pedido) {
     this.#pedido = pedido;
