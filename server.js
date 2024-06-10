bd43b1a311c9584a10af6069eccc316fc9f1dcd4
moradan <realmoradan@hotmail.com>
2024-06-10 13:40:06 -0300

    reescribo el server con express

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
+const { log } = require("console");
 const express = require("express");
+const http = require("http");
 
 const servidor = express();
 
 const PORT = 3000;
 
-servidor.listen(PORT, procesador);
+servidor.get("/", procesar);
+servidor.listen(PORT, inicio);
 
-function procesador(pedido, respuesta) {}
+function inicio() {
+  log("Escuchando");
+}
+
+/**
+ *
+ * @param {http.IncomingMessage} pedido
+ * @param {http.ServerResponse} respuesta
+ */
+function procesar(pedido, respuesta) {
+  log("Recibimos un pedido", pedido);
+  respuesta.setHeader("Status", 200);
+  respuesta.setHeader("Content-Type", "text/html");
+  respuesta.end("Respuesta");
+}
