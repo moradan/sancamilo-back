14855a95014dbca4d2df27bf9851bd75af2cdf32
moradan <realmoradan@hotmail.com>
2024-06-11 10:44:43 -0300

    utilizo send de Express, en vez de ServerResponse.end

--- src/login.js
+++ src/login.js
@@ -5,9 +5,7 @@ const http = require("node:http");
  * @param {http.ServerResponse} respuesta
  */
 function login(pedido, respuesta) {
-    respuesta.setHeader("Status-Code", 200);
-    respuesta.setHeader("Content-Type", "text/plain");
-    respuesta.end("Todavia no podemos validar usuarios");
+    respuesta.send("Todavia no podemos validar usuarios");
 }
 
 module.exports = login;
