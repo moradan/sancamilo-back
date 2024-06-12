5ebfeaad4dd1f9b3b236a0001b76e2a7044edcbf
moradan <realmoradan@hotmail.com>
2024-06-12 13:47:12 -0300

    Implemento una simulacion de agregar usuario a la bd.

--- src/conexion_datos.js
+++ src/conexion_datos.js
@@ -15,31 +15,40 @@
  */
 
 class Conexion {
-    /**
-     * @type {Array<Usuario>}
-     */
-    #usuarios = [
-        {
-            id: 1,
-            nombreCompleto: "rodrigo",
-            sexo: "masculino",
-            fechaNacimiento: "25-9-1981",
-            email: "realmoradan@hotmail.com",
-            prepaga: 1,
-            especialidad: null,
-            password: "password"
-        }
-    ];
-
-    /**
-     * Busca un usuario en la base de datos por nombre. 
-     * @param {string} nombreParaBuscar  El nombre completo del usuario que queremos buscar en la bd.
-     * @returns {Usuario | undefined} El objeto de tipo usuario que esta en la base de datos y coincide con el nombre provisto.
-     * Devuelve undefined si no encuentra un registro con ese nombre.
-     */
-    usuarioPorNombre(nombreParaBuscar) {
-        return this.#usuarios.find((usuario) => usuario.nombreCompleto === nombreParaBuscar);
+  /**
+   * @type {Array<Usuario>}
+   */
+  #usuarios = [
+    {
+      id: 1,
+      nombreCompleto: "rodrigo",
+      sexo: "masculino",
+      fechaNacimiento: "25-9-1981",
+      email: "realmoradan@hotmail.com",
+      prepaga: 1,
+      especialidad: null,
+      password: "password"
     }
+  ];
+
+  /**
+   * Busca un usuario en la base de datos por nombre. 
+   * @param {string} nombreParaBuscar  El nombre completo del usuario que queremos buscar en la bd.
+   * @returns {Usuario | undefined} El objeto de tipo usuario que esta en la base de datos y coincide con el nombre provisto.
+   * Devuelve undefined si no encuentra un registro con ese nombre.
+   */
+  usuarioPorNombre(nombreParaBuscar) {
+    return this.#usuarios.find((usuario) => usuario.nombreCompleto === nombreParaBuscar);
+  }
+
+  /**
+   * Agrega un nuevo usuario a la base de datos. 
+   * @param {Usuario} usuarioParaAgregar El objeto usuario que hay que agregar a la base de datos. Por el momento vamos a agregar
+   * sin preocuparnos por la creacion o prevencion de duplicados.
+   */
+  agregarUsuario(usuarioParaAgregar) {
+    this.#usuarios.push(usuarioParaAgregar);
+  }
 }
 
 
