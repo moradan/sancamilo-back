fd836011c3290bc32571d5a6c01174ef9790634f
moradan <realmoradan@hotmail.com>
2024-06-19 16:36:01 -0300

    creo una conexion mysql con mysql2

---
+++ src/conexion_sql.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql2");
+
+const conexion_sql = mysql.createConnection({
+    host: 'mysql-moradandb.alwaysdata.net',
+    user: 'moradandb',
+    password: 'cac2024',
+    database: 'moradandb_sancamilo'
+});
+
+conexion_sql.connect(callback);
+
+function callback(error) {
+    console.log("Intentando conectarse a alwaysdata.");
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    console.log("conectado a bs");
+}
+
+module.exports = conexion_sql;
