6e9389e47bca43c00f72b849c6d934e1e939e149
moradan <realmoradan@hotmail.com>
2024-06-21 17:51:33 -0300

    Modifico las rutas que deben usar metodo post.
    Tambien modifico la ruta para login para que tome dos parametros :email y :password.

--- src/rutas/rutasUsuarios.js
+++ src/rutas/rutasUsuarios.js
@@ -2,9 +2,9 @@ const express = require("express");
 const rutas = express.Router();
 const controlador = require("../controladores/controladorUsuarios");
 
-rutas.get("/login", controlador.login);
-rutas.get("/registrarse", controlador.registrarse);
+rutas.post("/login/:email/:password", controlador.login);
+rutas.post("/registrarse", controlador.registrarse);
 rutas.get("/profesionales", controlador.profesionales);
-rutas.get("/todos", controlador.todos);
+rutas.get("/", controlador.todos);
 
 module.exports = rutas;
