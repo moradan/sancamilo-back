bdbbf6ddefbd481b67fb9bb02860bda2bfda5525
moradan <realmoradan@hotmail.com>
2024-06-21 10:53:23 -0300

    Modifico server para responder a las nuevas rutas.
    Tambien vuelvo a escribir la ruta que responde a un pedido a la raiz del server '/'.

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 
 const rutasUsuarios = require("./src/rutas/rutasUsuarios");
+const rutasEspecialidades = require("./src/rutas/rutasEspecialidades");
 
 const servidor = express();
 
 const PORT = 3000;
 
+servidor.get('/', (req, res) => { res.status(200).json({ mensaje: "Api de SanCamilo" }) });
 servidor.use('/usuarios', rutasUsuarios);
+servidor.use('/especialidades', rutasEspecialidades);
 servidor.listen(PORT, inicio);
 
 function inicio() {
