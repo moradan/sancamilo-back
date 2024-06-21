4878dbf043cae553ca3785d08c7a8e654f71f489
moradan <realmoradan@hotmail.com>
2024-06-21 10:52:39 -0300

    implemento la primer ruta para especialidades
    posiblemente la unica.

--- src/rutas/rutasEspecialidades.js
+++ src/rutas/rutasEspecialidades.js
@@ -1,2 +1,7 @@
+const express = require("express");
+const rutas = express.Router();
+const controlador = require("../controladores/controladorEspecialidades");
 
-rutas.get("/especialidades", especialidades);
+rutas.get("/todos", controlador);
+
+module.exports = rutas;
