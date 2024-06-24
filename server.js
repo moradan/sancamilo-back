c7a60f27b6b4eb6f5a1a9be0b4f194e2988158b8
moradan <realmoradan@hotmail.com>
2024-06-24 10:09:10 -0300

    extraigo el manejo de la ruta raiz a una funcion

--- server.js
+++ server.js
@@ -8,12 +8,20 @@ const servidor = express();
 const PORT = 3000;
 
 servidor.use(express.json());
-servidor.get('/', (req, res) => { res.status(200).json({ mensaje: "Api de SanCamilo" }) });
+
+servidor.get('/', rutaRaiz);
 servidor.use('/usuarios', rutasUsuarios);
 servidor.use('/especialidades', rutasEspecialidades);
 servidor.use('/prepagas', rutasPrepagas);
 servidor.listen(PORT, inicio);
 
 function inicio() {
-  console.log("Escuchando");
+  console.log(`Escuchando en puerto: ${PORT}`);
+}
+
+/**
+ * @type {import("express").RequestHandler} 
+ */
+function rutaRaiz(pedido, respuesta) {
+  respuesta.status(200).json({ mensaje: "Api de SanCamilo" })
 }
