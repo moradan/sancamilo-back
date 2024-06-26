bcb73631916f63ae453ce20383ab7a70075d73fd
moradan <realmoradan@hotmail.com>
2024-06-26 19:33:45 -0300

    Muevo la autenticacion al controlador de usuarios.

--- src/controladores/controladorUsuarios.js
+++ src/controladores/controladorUsuarios.js
@@ -210,6 +210,41 @@ function modificarUsuario(pedido, respuesta) {
     });
 }
 
+/** @type {import("express").RequestHandler} */
+function autenticar(pedido, respuesta, proximo) {
+    const { email, password } = pedido.body;
+
+    if (!email || !password) {
+        respuesta.status(400).json({ message: "el pedido no incluye todos los campos necesarios" });
+        return;
+    }
+
+    /** @type {string} */
+    const comandoSql = "SELECT * FROM usuarios WHERE email = ?";
+    conexion.query(comandoSql, [email], (error, resultado) => {
+        if (error) {
+            console.log(error);
+            respuesta.status(500).json({ mensaje: "No se pudo completar la busqueda." });
+            return;
+        }
+
+        if (resultado.length > 1) {
+            console.log(resultado);
+            respuesta.status(500).json({ mensaje: "Hay varios registros con el mismo email y solo deberia haber 1." });
+            return;
+        }
+
+        const usuarioBuscado = resultado[0];
+        if (usuarioBuscado && bcrypt.compareSync(password, usuarioBuscado.password)) {
+            const token = jwt.sign({ id: usuarioBuscado.id }, process.env.CLAVE_SECRETA, { expiresIn: process.env.VENCIMIENTO_TOKEN });
+            respuesta.status(200).json({ auth: true, token });
+        } else {
+            respuesta.status(401).json({ message: "Nombre de usuario o contrasenia invalidos." });
+        }
+        return;
+    });
+}
+
 module.exports = {
     todos,
     profesionales,
@@ -217,4 +252,5 @@ module.exports = {
     filtrarDuplicados,
     borrarUsuario,
     modificarUsuario,
+    autenticar,
 }
--- src/rutas/rutasUsuarios.js
+++ src/rutas/rutasUsuarios.js
@@ -2,6 +2,7 @@ const express = require("express");
 const rutas = express.Router();
 const controlador = require("../controladores/controladorUsuarios");
 
+rutas.get("/login", controlador.autenticar);
 rutas.post("/registrarse", controlador.filtrarDuplicados, controlador.registrarse);
 rutas.put("/:id", controlador.modificarUsuario);
 rutas.get("/profesionales", controlador.profesionales);
