24b5ade950c810985a2a53e93774dd960210abda
moradan <realmoradan@hotmail.com>
2024-06-27 14:02:28 -0300

    Middleware autenticacion completo.

--- src/middleware/middlewareAutenticacion.js
+++ src/middleware/middlewareAutenticacion.js
@@ -1,11 +1,28 @@
-const conexion = require("../conexiones/conexion_sql");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
 
 /** @type {import("express").RequestHandler} */
-function autenticar(pedido, respuesta) {
-    console.log("Se intento acceder a una ruta protegida, aun no implementado.");
-    respuesta.status(501).json({ mensaje: "Todavia no se puede acceder a areas protegidas." });
+function autenticar(pedido, respuesta, proximo) {
+    const encabezadoAutorizacion = pedido.headers["authorization"];
+    if (!encabezadoAutorizacion) {
+        console.log("Se intento acceder a contenido protegido sin proveer un token.");
+        respuesta.status(401).json({ autenticado: false, mensaje: "Este pedido requiere un token y no enviaste un token." });
+        return;
+    }
+
+    const [tipoAutenticacion, token] = encabezadoAutorizacion.split(" ");
+    if (tipoAutenticacion === "Bearer") {
+        try {
+            const contenido = jwt.verify(token, process.env.CLAVE_SECRETA);
+            console.log(`Usuario autentificado id: ${contenido.id}`);
+            proximo();
+        } catch (error) {
+            console.error(error);
+        }
+    } else {
+        console.log("Se intento acceder a un recurso protegido con un tipo de autenticacion que no soportamos o sin proveer una clave.");
+        respuesta.status(501).json({ mensaje: "Esta intentando acceder a contenido protegido con autenticacion que no soportamos o sin proveer una clave." });
+        return;
+    }
 }
 
 module.exports = autenticar;
